a6d6c0d206bacbef84c048974dbdd3316fe9e6f2
Amanda Pollard <Amandappollard@gmail.com>
2025-01-16 19:29:48 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to DB: ", mongoose.connection.readyState); // should say, Connected to DB: 1
+}).catch(err => console.error(err));
+
+console.log("hello");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +31,18 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+let urlSchema = new mongoose.Schema({
+  original: {type: String, required: true},
+  short: Number
+});
+
+let Url = mongoose.model("Url", urlSchema);
+let resObj = {}
+
+app.post("/api/shorturl", bodyParser.urlencoded({ extended: false }), (req, res) => {
+  let urlEntered = req.body["url"]
+  resObj["original_url"] = urlEntered
+
+  res.json(resObj)
+})
